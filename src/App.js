8654406d69e31b90c651363f9a55e495e8afea05
ohjiyeon3275 <ojy940716@gmail.com>
2021-09-02 13:08:10 +0900

    little bit 수정함

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>안녕 승태 조금 수정했어</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        
       </header>
     </div>
   );
